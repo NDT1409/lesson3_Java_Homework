e01ef284a770680735f2f066ddd7c8e47f8d9429
NDT1409 <124195943+NDT1409@users.noreply.github.com>
2023-02-16 04:55:12 +0100

    Add files via upload

---
+++ lesson3_home_work/src/game/hero/Elf.java
@@ -0,0 +1,17 @@
+package game.hero;
+
+public class Elf {
+
+    public static void main(String[] args) {
+
+        private int id;
+        int id1;
+        int id2;
+        int id3;
+        int id4;
+        int id5;
+    }
+
+}
+// Простите так и не смог понять как сделать ДЗ с 11 пункта и 2-ую часть задания
+// Также не очень смог понять условия задания.
